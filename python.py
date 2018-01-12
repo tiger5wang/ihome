e69b2c8d58e0025508770ff19038229fb264b338
wangyuhu <sjzwangyuhu@163.com>
2018-01-12 16:25:50 +0800

    wangyuhu第二次修改

--- python.py
+++ python.py
@@ -1,7 +1,9 @@
 # coding:utf-8
-print("hello python")
 
 print("hello python")
 print("hello python")
 print("hello python")
 print("hello python")
+print("world hello")
+print("world hello")
+print("world hello")
